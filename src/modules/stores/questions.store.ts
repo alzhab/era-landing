dd9677e95d61bf553d0cca0633084ef5f60f0bfc
alzhab <abd.alz.b@gmail.com>
2021-10-03 17:15:22 +0600

    fix close modal

--- src/modules/stores/questions.store.ts
+++ src/modules/stores/questions.store.ts
@@ -82,6 +82,7 @@ export default class QuestionsStore extends BaseStore {
         }),
       success: () => {
         this.submitDataSuccess = true;
+        this.modalOpen = false;
         this.clear();
       },
     });
